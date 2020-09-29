f7abc948c778e5789e61937fab67b4c5983bafe5
hmallen <allenhm@gmail.com>
2020-09-29 00:47:59 -0600

    Working on subprocess integration. Launch not functional.

--- launcher.py
+++ launcher.py
@@ -1,8 +1,9 @@
 import configparser
 import logging
 import subprocess
+import sys
 
-from ballisticchat import socketfeed, monitor, dashboard
+# from ballisticchat import socketfeed, monitor, dashboard
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -11,7 +12,7 @@ logger.setLevel(logging.DEBUG)
 config = configparser.RawConfigParser()
 
 
-def launch_modules(config):
+def launch_modules():
     # Socket.io chat feed dump to MongoDB
     #
 
@@ -20,25 +21,36 @@ def launch_modules(config):
 
     # Display dashboard in terminal
     #
+    socket_proc = subprocess.Popen(['./dashboard/socketfeed.py'])
+    monitor_proc = subprocess.Popen(['./dashboard/monitor.py'])
+    dash_proc = subprocess.Popen(
+        ['./dashboard/dashboard.py'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
 
-    pass
+    out, err = dash_proc.communicate()
+    #logger.debug(f'out: {out}')
+    #logger.debug(f'err: {err}')
 
 
 if __name__ == '__main__':
-    import argparse
+    # import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--config', '-c', type=str,
-                        default='./config/settings.conf')
-    args = parser.parse_args()
-    config_path = args.config
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--config', '-c', type=str,
+    #                    default = './config/settings.conf')
+    # args = parser.parse_args()
+    # config_path = args.config
 
-    config.read(config_path)
+    # config.read(config_path)
 
     try:
-        # launch_modules(config)
-
-        socket_proc = subprocess.Popen(['ballisticchat/socketfeed.py'], )
+        launch_modules()
 
     except KeyboardInterrupt:
         logger.info('Exit signal received.')
+        sys.exit()
+
+    except Exception as e:
+        logger.exception(e)
